f4a9362be2e63dd77d1b148e587684805ecc2cee
Haroon <haroon.malik77@gmail.com>
2022-09-22 10:03:44 +0100

    small fixes, added code

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,35 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // current issues:
+        //1. not checking for non int number input, will throw exception for anything nthats not an int.
+        //2. when number not input outwit=dth range (1-100), i should catch and either let user correct or handle it some other way.
 
-        get10Numbers(9);    }
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers = get10Numbers(9);
+        Collections.sort(numbers, Collections.reverseOrder());
+
+        //Not asked for, but ive outputted the list to console using the below code
+        for(int str: numbers){
+            System.out.println(str);
+        }
+    }
 
     public static ArrayList<Integer> get10Numbers(int number) {
+        //there is an issue when inputting non integers.
+        //given time id add an exception handler and/or possibly function to check input before addign to array list
         int i, j;
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         Scanner getInput = new Scanner(System.in);
-        System.out.println("Input 10 numbers between 1 and 100 : ");
-        j = getInput.nextInt();
+
         for (i=0; i<10; i++) {
-            if (j>=1 && j<=100) {
-                numbers.add(getInput.nextInt());
-            } else {
-                System.out.println("Number not between 1 and 100");
-                i = i-1;
+            System.out.println("Input 10 numbers between 1 and 100 : ");
+            j = getInput.nextInt();
+            if (j>=1 && j<=100 && !numbers.contains(j)) {
+                numbers.add(j);
             }
         }
         return numbers;
